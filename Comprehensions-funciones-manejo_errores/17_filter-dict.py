7c694ca7d85741186e5aca9b593490246e0f2c1b
chalo <lpalaciosayala@gmail.com>
2025-08-14 21:16:43 -0500

    Auto-commit 2025-08-14

---
+++ Comprehensions-funciones-manejo_errores/17_filter-dict.py
@@ -0,0 +1,45 @@
+
+matches = [
+  {
+    'home_team': 'Bolivia',
+    'away_team': 'Uruguay',
+    'home_team_score': 3,
+    'away_team_score': 1,
+    'home_team_result': 'Win'
+  },
+  {
+    'home_team': 'Brazil',
+    'away_team': 'Mexico',
+    'home_team_score': 1,
+    'away_team_score': 1,
+    'home_team_result': 'Draw'
+  },
+  {
+    'home_team': 'Ecuador',
+    'away_team': 'Venezuela',
+    'home_team_score': 5,
+    'away_team_score': 0,
+    'home_team_result': 'Win'
+  },
+]
+
+print(matches)
+print(len(matches))
+
+new_list = list(filter(lambda item: item['home_team_result'] == 'Win', matches))
+
+print(new_list)
+print(len(new_list))
+
+print(matches)
+print(len(matches))
+
+#prueba
+
+matches = [
+    {"team": "A", "result": "win"},
+    {"team": "B", "result": "draw"},
+    {"team": "C", "result": "win"}
+]
+ganados = list(filter(lambda m: m["result"] == "win", matches))
+print(ganados)
