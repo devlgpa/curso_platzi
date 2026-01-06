4d2cb035bedc40c4cec5287343f5724d0ef8fc39
Chalo <lpalaciosayala@gmail.com>
2026-01-05 21:39:53 -0500

    5-1-26

---
+++ Comprehensions-funciones-manejo_errores/20-iter.py
@@ -0,0 +1,12 @@
+
+for i in range(1, 11):
+    print(i)
+
+my_iter = iter(range(1, 11))
+print(my_iter)
+
+#forma manual de iterar
+print(next(my_iter))
+print(next(my_iter)) 
+print(next(my_iter))
+print(next(my_iter))   
